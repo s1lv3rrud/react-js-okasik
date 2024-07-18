d3522ba890f29d223f46634a05a34946638a9031
s1lv3rrud <lek8575@naver.com>
2024-07-18 23:36:50 +0900

    chore: CSS 파일명 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/main.css";
+import "../styles/Home.css";
 
 const generateStars = (count) => {
   const stars = [];
