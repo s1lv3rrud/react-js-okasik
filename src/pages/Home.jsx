b8429e068a8cde85d1c4655a4769636588af8d76
s1lv3rrud <lek8575@naver.com>
2024-07-24 12:28:29 +0900

    chore: 필요없는 CSS 코드 제거

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/Home.css";
 
