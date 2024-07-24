9b56c1b36306b476ff3049b1276beade45d800c5
s1lv3rrud <lek8575@naver.com>
2024-07-24 12:20:03 +0900

    feat: 랜덤 캐릭터 고정

--- src/components/CategoryStoryCard.jsx
+++ src/components/CategoryStoryCard.jsx
@@ -2,35 +2,9 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/CategoryStoryCard.css";
-import man01 from "../assets/images/man01.jpg";
-import man02 from "../assets/images/man02.jpg";
-import man08 from "../assets/images/man08.jpg";
-import man03 from "../assets/images/man03.webp";
-import man04 from "../assets/images/man04.webp";
-import man05 from "../assets/images/man05.webp";
-import man06 from "../assets/images/man06.webp";
-import man07 from "../assets/images/man07.webp";
-import man09 from "../assets/images/man09.webp";
-import man10 from "../assets/images/man10.webp";
-import man11 from "../assets/images/man11.webp";
 
-const images = [
-  man01,
-  man02,
-  man03,
-  man04,
-  man05,
-  man06,
-  man07,
-  man08,
-  man09,
-  man10,
-  man11,
-];
-
-const CategoryStoryCard = () => {
+const CategoryStoryCard = ({ image }) => {
   const navigate = useNavigate();
-  const randomImage = images[Math.floor(Math.random() * images.length)];
 
   const [activeButtons, setActiveButtons] = useState(["전체"]);
 
@@ -52,7 +26,7 @@ const CategoryStoryCard = () => {
   return (
     <div className="card">
       <div className="card-img-container">
-        <img src={randomImage} className="card-img-top" />
+        <img src={image} className="card-img-top" />
       </div>
       <div className="card-body">
         <p className="card-text">
--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -1,10 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/Story.css";
 import CategoryStoryCard from "../components/CategoryStoryCard";
 import StoryCard from "../components/StoryCard";
 import ResultStoryCard from "../components/ResultStoryCard";
+import man01 from "../assets/images/man01.jpg";
+import man02 from "../assets/images/man02.jpg";
+import man08 from "../assets/images/man08.jpg";
+import man03 from "../assets/images/man03.webp";
+import man04 from "../assets/images/man04.webp";
+import man05 from "../assets/images/man05.webp";
+import man06 from "../assets/images/man06.webp";
+import man07 from "../assets/images/man07.webp";
+import man09 from "../assets/images/man09.webp";
+import man10 from "../assets/images/man10.webp";
+import man11 from "../assets/images/man11.webp";
+
+const images = [
+  man01,
+  man02,
+  man03,
+  man04,
+  man05,
+  man06,
+  man07,
+  man08,
+  man09,
+  man10,
+  man11,
+];
 
 const generateStars = (count) => {
   const stars = [];
@@ -19,13 +44,21 @@ const generateStars = (count) => {
 };
 
 const Story = () => {
+  const randomImage = images[Math.floor(Math.random() * images.length)];
+
   return (
     <div className="background d-flex flex-column justify-content-center align-items-center vh-100">
       {generateStars(100)}
       <Routes>
-        <Route path="/category" element={<CategoryStoryCard />} />
-        <Route path="/story" element={<StoryCard />} />
-        <Route path="/result" element={<ResultStoryCard />} />
+        <Route
+          path="/category"
+          element={<CategoryStoryCard image={randomImage} />}
+        />
+        <Route path="/story" element={<StoryCard image={randomImage} />} />
+        <Route
+          path="/result"
+          element={<ResultStoryCard image={randomImage} />}
+        />
       </Routes>
     </div>
   );
