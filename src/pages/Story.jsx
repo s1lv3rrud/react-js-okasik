85553aed12e4af830b7e3808f9c8cbdac1190707
s1lv3rrud <lek8575@naver.com>
2024-07-25 12:42:08 +0900

    chore: 이미지 변경
    - AI 생성 이미지 손가락 개수 이슈

--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -7,15 +7,10 @@ import StoryCard from "../components/StoryCard";
 import ResultStoryCard from "../components/ResultStoryCard";
 import man01 from "../assets/images/man01.jpg";
 import man02 from "../assets/images/man02.jpg";
-import man08 from "../assets/images/man08.jpg";
 import man03 from "../assets/images/man03.webp";
 import man04 from "../assets/images/man04.webp";
 import man05 from "../assets/images/man05.webp";
 import man06 from "../assets/images/man06.webp";
-import man07 from "../assets/images/man07.webp";
-import man09 from "../assets/images/man09.webp";
-import man10 from "../assets/images/man10.webp";
-import man11 from "../assets/images/man11.webp";
 import woman01 from "../assets/images/woman01.jpg";
 import woman02 from "../assets/images/woman02.jpg";
 import woman03 from "../assets/images/woman03.jpg";
@@ -23,9 +18,6 @@ import woman04 from "../assets/images/woman04.jpg";
 import woman05 from "../assets/images/woman05.jpg";
 import woman06 from "../assets/images/woman06.jpg";
 import woman07 from "../assets/images/woman07.jpg";
-import woman08 from "../assets/images/woman08.jpg";
-import woman09 from "../assets/images/woman09.jpg";
-import woman10 from "../assets/images/woman10.jpg";
 
 const images = [
   man01,
@@ -34,11 +26,6 @@ const images = [
   man04,
   man05,
   man06,
-  man07,
-  man08,
-  man09,
-  man10,
-  man11,
   woman01,
   woman02,
   woman03,
@@ -46,9 +33,6 @@ const images = [
   woman05,
   woman06,
   woman07,
-  woman08,
-  woman09,
-  woman10,
 ];
 
 const generateStars = (count) => {
