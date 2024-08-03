02dfcde2f847a77ce1677470cf967b24ee22dd47
s1lv3rrud <lek8575@naver.com>
2024-08-03 18:55:32 +0900

    feat: 메인화면 로그아웃 버튼 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { loginSuccess } from "../features/auth/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { loginSuccess, logout } from "../features/auth/authSlice";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/Home.css";
 import Layout from "../components/Layout";
 
 const Home = () => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const navigate = useNavigate();
   const location = useLocation();
   const dispatch = useDispatch();
@@ -32,27 +33,58 @@ const Home = () => {
     }
   }, [location, dispatch, navigate]);
 
+  const handleKakaoLogout = () => {
+    const BASE_URL = import.meta.env.VITE_BASE_URL;
+    window.location.href = `${BASE_URL}/v1/logout/kakao`;
+
+    // Redux 상태 업데이트 및 로그아웃 처리
+    dispatch(logout());
+    alert("로그아웃 되었습니다.");
+    navigate("/");
+  };
+
   return (
     <Layout>
       <div className="d-flex flex-column justify-content-center align-items-center vh-100">
         <h1 className="logo-text text-left">잘먹어요</h1>
         <h1 className="logo-text text-right">아가씨</h1>
         <div className="mb-4"></div>
-        <button type="button" className="btn login-button">
-          <img
-            onClick={handleKakaoLogin}
-            src="https://k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
-            alt="카카오 로그인"
-            className="kakao-login-button-image"
-          />
-        </button>
-        <button
-          type="button"
-          className="btn btn-light login-button"
-          onClick={() => navigate("/story/category")}
-        >
-          🧑‍🍳 로그인 없이 시작하기
-        </button>
+        {isLoggedIn ? (
+          <div>
+            <button
+              type="button"
+              className="btn btn-light login-button"
+              onClick={handleKakaoLogout}
+            >
+              🎬 시작하기
+            </button>
+            <button
+              type="button"
+              className="btn btn-light login-button"
+              onClick={handleKakaoLogout}
+            >
+              ⮐ 로그아웃
+            </button>
+          </div>
+        ) : (
+          <div>
+            <button type="button" className="btn login-button">
+              <img
+                onClick={handleKakaoLogin}
+                src="https://k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
+                alt="카카오 로그인"
+                className="kakao-login-button-image"
+              />
+            </button>
+            <button
+              type="button"
+              className="btn btn-light login-button"
+              onClick={() => navigate("/story/category")}
+            >
+              🧑‍🍳 로그인 없이 시작하기
+            </button>
+          </div>
+        )}
       </div>
     </Layout>
   );
