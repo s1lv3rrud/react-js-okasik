9f0502f0d9c918ceba2a87bbac98528917efc4fe
s1lv3rrud <lek8575@naver.com>
2024-08-04 16:25:03 +0900

    design: story 화면 자동 넘김

--- src/components/StoryCard.jsx
+++ src/components/StoryCard.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/StoryCard.css";
+import LoadingSpinner from "../assets/LoadingSpinner.gif";
 
 const StoryCard = () => {
   const navigate = useNavigate();
@@ -9,10 +10,19 @@ const StoryCard = () => {
   const { recommendation, image } = location.state || {};
   const isWoman = image?.includes("woman");
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/story/result", { state: { recommendation, image } });
+    }, 1000);
+
+    // 타이머를 클린업하여 컴포넌트가 언마운트될 때 메모리 누수를 방지
+    return () => clearTimeout(timer);
+  }, [navigate, recommendation, image]);
+
   return (
     <div className="card">
       <div className="card-img-container">
-        <img src={image} className="card-img-top" />
+        <img src={image} className="card-img-top" alt="Story" />
       </div>
       <div className="card-body">
         <p className="card-text">
@@ -20,13 +30,8 @@ const StoryCard = () => {
           <br />또 메뉴 정하는 거예요?
         </p>
       </div>
-      <div
-        className="card-footer btn"
-        onClick={() =>
-          navigate("/story/result", { state: { recommendation, image } })
-        }
-      >
-        다음으로
+      <div className="card-footer loading-button">
+        <img src={LoadingSpinner} alt="Loading..." />
       </div>
     </div>
   );
